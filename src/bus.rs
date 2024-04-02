6e3d4dcda32775545cd25631cdf41471f4b8ea8f
Zhang Mingzhe <1245539137@qq.com>
2024-04-03 06:59:37 +0800

    add clint plic module

--- src/bus.rs
+++ src/bus.rs
@@ -1,18 +1,28 @@
 use crate::param::*;
 use crate::dram::Dram;
+use crate::plic::Plic;
+use crate::clint::Clint;
 use crate::exception::*;
 
 pub struct Bus{
-    dram:Dram
+    dram:Dram,
+    plic:Plic,
+    clint:Clint,
 }
 
 impl Bus {
    pub fn new(code:Vec<u8>) -> Bus{
-        Self{dram: Dram::new(code)}
+        Self {
+            dram: Dram::new(code),
+            clint: Clint::new(),
+            plic:Plic::new(),
+        }
    }
 
    pub fn load(&mut self, addr:u64, size:u64)->Result<u64,Exception>{
     match addr {
+        CLINT_BASE..=CLINT_END => self.clint.load(addr, size),
+        PLIC_BASE..=PLIC_END=> self.plic.load(addr, size),
         DRAM_BASE..=DRAM_END => self.dram.load(addr, size),
         _ => Err(Exception::LoadAccessFault(addr)),
     }
@@ -20,6 +30,8 @@ impl Bus {
 
    pub fn store(&mut self, addr: u64, size:u64, value:u64)->Result<(), Exception>{
     match addr {
+        CLINT_BASE..=CLINT_END=>self.clint.store(addr, size, value),
+        PLIC_BASE..=PLIC_END=>self.plic.store(addr, size, value),
         DRAM_BASE..=DRAM_END => self.dram.store(addr, size, value),
         _ => Err(Exception::StoreAMOAccessFault(addr)),
     }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod bus;
 mod cpu;
 mod exception;
 mod csr;
+mod plic;
+mod clint;
 
 fn main() {
     println!("Hello, world!");
